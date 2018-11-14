ce090097b42fe7f17db380d6e62a4af795efb10d
andy reyes <11710reyes@gmail.com>
2018-11-14 20:21:18 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = { Slowdive: '', MyBloodyValentine: 'Sometimes'} 
+var playlist = { Slowdive: 'Alison', MyBloodyValentine: 'Sometimes'} 
 
 function updatePlaylist (playlist, artistName, songTitle) {
   playlist['Phil Ochs'] = "Here's to the State of Mississippi"
