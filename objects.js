7fd21e236b0d1fbf6958c892dda0089a812bb94a
andy reyes <11710reyes@gmail.com>
2018-11-14 20:14:46 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = { Slowdive: 'Alison', MyBloodyValentine: 'Sometimes'} 
 
 function updatePlaylist (playlist, artistName, songTitle) {
-  playlist.
+  playlist['Phil Ochs'] = 
   return playlist
 }
 
